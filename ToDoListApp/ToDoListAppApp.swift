2fe9c731873bcb3f580fb7bbfdb4d8997de0b10f
Virinda Popli <poplivirinda0@gmail.com>
2025-07-14 09:59:13 -0400

    Initial Commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by Scholar on 7/14/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
